eb53b5c3b0eb6bd10302cf1fb1fd57f111ee542b
Coutana <1948835139@qq.com>
2023-10-29 23:05:23 +0800

    Initial commit

---
+++ src/main/java/com/zzu/vedio/VedioApplication.java
@@ -0,0 +1,13 @@
+package com.zzu.vedio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VedioApplication.class, args);
+	}
+
+}
